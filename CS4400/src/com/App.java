4dde154ed07640e1fbc71ebf965d3ec6fe869163
Bryan <bzhang57@gatech.edu>
2018-04-18 20:23:22 -0400

    pushing frontend

---
+++ CS4400/src/com/App.java
@@ -0,0 +1,33 @@
+package com;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by bzhang345 on 4/18/18.
+ */
+public class App {
+    private JButton Login;
+    private JPanel panel1;
+
+    public App() {
+        Login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Hello");
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
