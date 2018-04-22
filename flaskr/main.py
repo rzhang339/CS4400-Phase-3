1b63a83a377d75988284fefdd1b32ae543e0d845
Raymond Z <rzhang339@gmail.com>
2018-04-22 02:18:36 -0400

    asldfkjadlfj

--- flaskr/main.py
+++ flaskr/main.py
@@ -8,7 +8,14 @@ import Visits
 
 from flaskr import app
 
+@app.after_request
+def after_request(response):
+	response.headers.add('Access-Control-Allow-Origin', '*')
+	response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+	response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+	return response
 
+@app.after_request
 def apply_caching(response):
     response.headers["Access-Control-Allow-Credentials"] = "true"
     return response
