8c0f2713872e7c6994af566e15aec188b19f72d7
Raymond Z <rzhang339@gmail.com>
2018-04-22 11:55:58 -0400

    session stuff

--- flaskr/User.py
+++ flaskr/User.py
@@ -2,6 +2,7 @@ import pymysql
 from flask import request, session, Response
 import hashlib
 from flaskr import app, db
+import json
 
 class User():
 
@@ -65,10 +66,19 @@ class User():
             session.pop('user', None)
             return "logout"
 
+
+    @staticmethod
+    def get_session():
+        if 'user' in session.keys():
+            return json.dumps(session['user'], sort_keys=True, indent=4, separators=(',', ': '))
+        else:
+            return "not logged in"
+
     def hash_password(password):
         return hashlib.md5(bytes(password.encode())).hexdigest()
 
 app.add_url_rule('/register', 'register', User.register, methods=['POST'])
 app.add_url_rule('/login', 'login', User.login, methods=['POST'])
 app.add_url_rule('/logout', 'logout', User.logout, methods=['GET'])
+app.add_url_rule('/get_session', 'get_session', User.get_session, methods=['GET'])
 
